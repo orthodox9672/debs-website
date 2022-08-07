379f149742bd468a4d3fd4ce8679574ba1bf69ed
orthodox9672 <110270141+orthodox9672@users.noreply.github.com>
2022-08-07 18:26:00 +1200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('nav_menu')[0]
+
+toggleButton.addEventListener('click', ()=>{
+    navbarLinks.classList.toggle('active')
+})
